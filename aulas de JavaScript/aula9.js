98d86b8ccfde27af39ee0574cf15b03ef691dacb
Andrei Cardozo <andrei.cardozo@unidavi.edu.br>
2024-09-11 16:12:26 -0300

    atividades

--- aulas de JavaScript/aula9.js
+++ aulas de JavaScript/aula9.js
@@ -255,3 +255,28 @@ function saudacao(nome){
     return mFrase;
 };
 console.log(saudacao('Andrei'));
+
+/*
+Pedido na pizzaria 🍕
+Crie uma função clientePedido que receba um parâmetro numeroPedido. Considere as condições a seguir:
+
+Se numeroPedido for igual a 1, retorne a mensagem 'Pizza de Calabresa';
+Se numeroPedido for igual a 2, retorne a mensagem 'Pizza de Quatro Queijos';
+Se numeroPedido for igual a 3, retorne a mensagem 'Pizza de Frango com Catupiry';
+Se numeroPedido for igual a 4, retorne a mensagem 'Pizza de Brigadeiro';
+Se numeroPedido não entrar nas condições acima, retorne a mensagem 'Número do pedido inválido!'.
+*/
+function clientePedido(numeroPedido) {
+    if (numeroPedido === 1) {
+        return 'Pizza de calabresa';
+    } else if (numeroPedido === 2) {
+        return 'Pizza de Quatro Queijos';
+    } else if (numeroPedido === 3) {
+        return 'Pizza de Frango com Catupiry';
+    } else if (numeroPedido === 4) {
+        return 'Pizza de Brigadeiro';
+    } else {
+        return 'Número de pedido inválido!';
+    }
+}
+console.log(clientePedido(1))
